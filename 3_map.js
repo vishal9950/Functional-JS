d8e48a3ea48739bf7dcafb99f2bb8434b8adcac8
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-16 12:05:30 +0530

    Ex3: True for proper args

---
+++ 3_map.js
@@ -0,0 +1,22 @@
+function doubleAll(numbers) {
+  const map1 = numbers.map(x => x * 2);
+  console.log(map1);
+  return map1;
+}
+
+//module.exports = doubleAll;
+function areArraysEqual(first, second) {
+  let flag = 1;
+  for (let i = 0; i < first.length; i++) {
+    if(first[i] != second[i]){
+      flag = 0;
+    }
+  }
+  if(flag === 1){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+console.log('#TC1: Should be true for proper args: ', areArraysEqual(doubleAll([1,2,3,4]), [ 2, 4, 6, 8]));
