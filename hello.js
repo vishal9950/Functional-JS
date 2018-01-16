45a567990feae63209bf99c23d7f27b3c5d9adf2
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-16 10:00:59 +0530

    True for random string input

--- hello.js
+++ hello.js
@@ -3,6 +3,8 @@ function upperCaser(a) {
     console.log('Err 1: Empty string supplied!');
     return false;
   }
+  return a.toUpperCase();
 }
 
 console.log('#TC1: Should be false for empty string: ', upperCaser('') === false);
+console.log('#TC2: Should be true for random strings: ', upperCaser('Lorem Ipsum') === 'LOREM IPSUM');
