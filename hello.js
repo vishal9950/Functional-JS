a01ea40cca5224a57a771276bfbb90d10b369bc6
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-16 10:10:21 +0530

    Verify with tool ex1

--- hello.js
+++ hello.js
@@ -6,6 +6,7 @@ function upperCaser(a) {
   return a.toUpperCase();
 }
 
+//module.exports = upperCaser;
 console.log('#TC1: Should be false for empty string: ', upperCaser('') === false);
 console.log('#TC2: Should be true for random strings: ', upperCaser('Lorem Ipsum') === 'LOREM IPSUM');
 console.log('#TC3: Should be true for all CAPS string: ', upperCaser('HELLO') === 'HELLO');
