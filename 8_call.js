427231d1c252339bccd14db728262a72714ec577
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-17 13:36:18 +0530

    Ex8: False for 0 args

---
+++ 8_call.js
@@ -0,0 +1,11 @@
+function duckCount() {
+  if(arguments.length === 0){
+    return false;
+  }
+	let result = {};
+
+}
+
+module.exports = duckCount;
+
+console.log('Should be false for no args: ', duckCount() === false);
