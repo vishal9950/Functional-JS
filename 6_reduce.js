db3c94877d0850a0dcfdbfc802c07d84d161076c
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-17 10:53:54 +0530

    Ex6: False for empty string

---
+++ 6_reduce.js
@@ -0,0 +1,14 @@
+function countWords(inputWords) {
+	if(inputWords.length === 0){
+		return false;
+	}
+	else {
+
+	}
+}
+
+module.exports = countWords;
+
+console.log('Should not work for empty array: ', countWords([]) === false);
+
+//console.log('Should work for proper args: ', countWords(['Apple', 'Banana', 'Apple', 'Durian', 'Durian', 'Durian']));
