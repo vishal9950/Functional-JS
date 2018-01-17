4041d5a128f9d6ccad7051fa99e3a3486e36c0f9
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-17 11:42:03 +0530

    Ex7: False for empty array

---
+++ 7_recursion.js
@@ -0,0 +1,9 @@
+function reduce(arr, fn, initial) {
+	if (arr.length === 0) {
+		return false;
+	}
+}
+
+module.exports = reduce;
+
+console.log('Should be false for empty array: ', reduce([]) === false);
