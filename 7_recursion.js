d3d397b0d1e2cc0a8cdcac96fbcdc93f41b4d60b
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-17 12:54:57 +0530

    Ex7: Return initial value for empty array

--- 7_recursion.js
+++ 7_recursion.js
@@ -1,9 +1,16 @@
-function reduce(arr, fn, initial) {
+function reduce(arr, fn, initial, ind=0) {
 	if (arr.length === 0) {
-		return false;
+		return initial;
+	}
+	else {
+		if(arr.length === ind) return initial;
+		let curr = fn(initial, arr[ind], ind, arr);
+    return reduce(arr, fn, curr, ++ind);
 	}
 }
 
 module.exports = reduce;
 
-console.log('Should be false for empty array: ', reduce([]) === false);
+console.log('Should be false for empty array: ', reduce([]) === undefined);
+
+// console.log('Should be true for proper args: ', reduce([]) === true);
