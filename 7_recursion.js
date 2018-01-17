5389cae285107b3f21f357923b61c615e3fcbb3a
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-17 13:06:42 +0530

    Ex7: True for Strings

--- 7_recursion.js
+++ 7_recursion.js
@@ -1,5 +1,6 @@
 function reduce(arr, fn, initial, ind=0) {
 	if (arr.length === 0) {
+    console.log('Err1: Empty array provided');
 		return initial;
 	}
 	if(arr.length === ind) return initial;
@@ -15,4 +16,6 @@ module.exports = reduce;
 
 console.log('Should be false for empty array: ', reduce([]) === undefined);
 
-console.log('Should be true for proper args: ', reduce([1, 2, 3, 4], add, 0) === 10);
+console.log('Should be true for numeric args: ', reduce([1, 2, 3, 4], add, 0) === 10);
+
+console.log('Should be true for strings: ', reduce(['World', '!'], add, 'Hello ') === 'Hello World!');
