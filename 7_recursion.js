2d7032404745e41dea38be726bb402d1778e841e
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-17 13:02:35 +0530

    Ex7: true for numeric arguments

--- 7_recursion.js
+++ 7_recursion.js
@@ -7,8 +7,12 @@ function reduce(arr, fn, initial, ind=0) {
   return reduce(arr, fn, curr, ++ind);
 }
 
+function add(prev, curr, indx, arr) {
+  return prev + curr;
+}
+
 module.exports = reduce;
 
 console.log('Should be false for empty array: ', reduce([]) === undefined);
 
-// console.log('Should be true for proper args: ', reduce([]) === true);
+console.log('Should be true for proper args: ', reduce([1, 2, 3, 4], add, 0) === 10);
