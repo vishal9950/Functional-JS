29141c39603457e1a1dd7d9f88aa4e053ec74599
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-19 01:39:25 +0530

    Ex10: Verified

---
+++ 10_bind.js
@@ -0,0 +1,11 @@
+module.exports = function(namespace) {
+	return console.log.bind(console, namespace);
+};
+
+// testcases
+
+
+//console.log('Should not work for empty arguments: ', false);
+// console.log('Should work for \'WARN\' and \'Info Msg\': ', false);
+// console.log('Should not work for \'\' and \'Info Msg\': ', false);
+// console.log('Should not work for \'WARN\' and \'\': ', false);
